e9dc7de58e5138f5230997b5c2f5e8be2d109ca4
Michael Peterson <WebDev@ctoms.ca>
2019-07-23 12:28:01 -0600

    setup basic structure of test file

---
+++ test/compareData.js
@@ -0,0 +1,11 @@
+const {Status} = require("../models/statusModel");
+
+function compareData(shopifyProducts,index,results){
+	var promise = new Promise((resolve,reject) => {
+		resolve(results);
+	});
+
+	return promise;
+}
+
+module.exports = {compareData};
---
+++ test/testData.js
@@ -0,0 +1,55 @@
+const chai = require('chai');
+const mongoose = require('mongoose');
+const expect = chai.expect;
+const {app, runServer, closeServer} = require('../server');
+const {GetData} = require("../classes/getData");
+const {compareData} = require('./compareData');
+const {DATABASE_URL,URLUS,USERK,USERP} = require('../config');
+
+describe('Test Old data with Shopify data',function(){
+	before(function(){
+		return runServer(DATABASE_URL);
+	});
+
+	after(function() {
+	    return closeServer();
+	});
+
+	it('should start server',function(done){
+		expect(1+1).to.equal(2);
+		done();
+	});
+
+	it('should get data',function(){
+		this.timeout(20000);
+		let getData = new GetData(URLUS,USERK,USERP);
+		return getData.getData([],0)
+
+		.then(productData => {
+			expect(productData).to.have.lengthOf.at.least(1);
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	});
+
+	it('all data should match',function(){
+		this.timeout(90000);
+		let getData = new GetData(URLUS,USERK,USERP);
+		return getData.getData([],0)
+
+		.then(productData => {
+			return compareData(productData,0,[])
+		})
+
+		.then(results => {
+			console.log("=========================results from compare prices: ", results.length);
+			expect(results).to.have.lengthOf(0);
+		})
+
+		.catch(err => {
+			console.log("Error testing: ",err);
+		});
+	});
+
+});
